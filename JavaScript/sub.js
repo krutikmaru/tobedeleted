2918c15a0d970ee72f63222cd4c9c526704e481a
krutikmaru <krutikmaru18@gmail.com>
2024-03-06 17:28:30 +0530

    Subtraction added

---
+++ JavaScript/sub.js
@@ -0,0 +1,10 @@
+const firstNumber = 5;
+const secondNumber = 6;
+
+console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+
+const getDocumentation = () => {
+  return `
+        first number - second number = result
+    `;
+};
