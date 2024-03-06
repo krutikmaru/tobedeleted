dd5b53ada8f085693f3f798fb539639dd9b0b104
krutikmaru <krutikmaru18@gmail.com>
2024-03-06 17:23:34 +0530

    Documentation

--- JavaScript/add.js
+++ JavaScript/add.js
@@ -2,3 +2,9 @@ const firstNumber = 5;
 const secondNumber = 6;
 
 console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+
+const getDocumentation = () => {
+  return `
+        first number + second number = result
+    `;
+};
